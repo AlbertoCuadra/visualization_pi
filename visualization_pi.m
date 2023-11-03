e647d565d62566923f094923ea10cb848c4be43c
AlbertoCuadra <acuadra@ing.uc3m.es>
2023-11-03 18:25:53 +0100

    Add: include color definitions

--- visualization_pi.m
+++ visualization_pi.m
@@ -9,9 +9,9 @@ function visualization_pi(varargin)
     %
     % Examples:
     %    visualization_pi('fps', 144);
-    %    visualization_pi('fps', 144, 'tiles', 20);
-    %    visualization_pi('fps', 144, 'tiles', 20, 'radius0', 0.5);
-    %    visualization_pi('fps', 144, 'tiles', 20, 'radius0', 0.5, 'radius1', 2); 
+    %    visualization_pi('fps', 144, 'points', 20);
+    %    visualization_pi('fps', 144, 'points', 20, 'radius0', 0.5);
+    %    visualization_pi('fps', 144, 'points', 20, 'radius0', 0.5, 'radius1', 2); 
     %
     % @author: Alberto Cuadra Lara
     %          Postdoctoral researcher - Group Fluid Mechanics
@@ -21,9 +21,13 @@ function visualization_pi(varargin)
 
     % Default
     FPS = 60;    % Frames per second
-    tiles = 5;   % Number of tiles to advance
+    points = 5;  % Number of points to advance
     radius0 = 1; % Radius for the inner curve
     radius1 = 1; % Radius for the outer curve
+    
+    % Miscellaneous
+    color_draw = [0, 0, 0];
+    color_arm = [1, 0.5961, 0.0902];
     
     % Get inputs
     for i = 1:2:nargin
@@ -31,8 +35,8 @@ function visualization_pi(varargin)
         switch lower(varargin{i})
             case 'fps'
                 FPS = varargin{i + 1};
-            case 'tiles'
-                tiles = varargin{i + 1};
+            case 'points'
+                points = varargin{i + 1};
             case 'radius0'
                 radius0 = varargin{i + 1};
             case 'radius1'
@@ -56,14 +60,15 @@ function visualization_pi(varargin)
     ylim([-1, 1] * (radius0 + radius1));
 
     % Initialization
+    timer_val = tic;
     running = true; 
     iteration = 1;
-    line0 = plot([0, 0], [0, 0], '-b', 'LineWidth', 1.5);
-    line1 = plot([0, 0], [0, 0], '-bo', 'LineWidth', 1.5);
+    line0 = plot([0, 0], [0, 0], '-', 'Color', color_arm, 'LineWidth', 1.5);
+    line1 = plot([0, 0], [0, 0], '-o', 'Color', color_arm, 'LineWidth', 1.5);
 
     while running
         % Define the range of values to plot
-        range = (iteration : (iteration + tiles)) / 180 * pi;
+        range = (iteration : (iteration + points)) / 180 * pi;
         
         % Get values
         real_part = real(z1(range));
@@ -73,21 +78,21 @@ function visualization_pi(varargin)
         imaginary_part0 = imag(z0(range(end)));
     
         % Plot
-        plot(real_part, imaginary_part, '-k');
+        plot(real_part, imaginary_part, '-', 'Color', color_draw);
 
         % Update the position of the point marker
         set(line0, 'XData', [0, real_part0], 'YData', [0, imaginary_part0]);
         set(line1, 'XData', [real_part0, real_part(end)], 'YData', [imaginary_part0, imaginary_part(end)]);
 
         % Update title
-        titletext = sprintf('Frame %d', round(range(end) * 180 / pi));
+        titletext = sprintf('Frame %d, Time %.2f [s]', round(range(end) * 180 / pi), toc(timer_val));
         title(titletext, 'Interpreter', 'latex', 'FontSize', 16)
 
         % Pause to control frame rate
         pause(1 / FPS);
     
         % Update the iteration counter
-        iteration = iteration + tiles;
+        iteration = iteration + points;
         
         % Check if the loop should stop
         if fig.UserData
